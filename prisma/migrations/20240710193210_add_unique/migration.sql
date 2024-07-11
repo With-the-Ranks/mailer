2ee93584a1196752d6165096679e55e0dabb4906
WTR Support <support@withtheranks.com>
2024-07-11 16:11:23 +0700

    Fix build erorr

--- prisma/migrations/20240710193210_add_unique/migration.sql
+++ prisma/migrations/20240710193210_add_unique/migration.sql
@@ -1,10 +1,8 @@
--- Ensure no duplicates exist before applying the constraint
-WITH duplicates AS (
-  SELECT id, ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) as row_num
-  FROM "Audience"
-)
-DELETE FROM "Audience"
-WHERE id IN (SELECT id FROM duplicates WHERE row_num > 1);
+/*
+  Warnings:
 
--- Apply the unique constraint
+  - A unique constraint covering the columns `[email]` on the table `Audience` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
 CREATE UNIQUE INDEX "Audience_email_key" ON "Audience"("email");
