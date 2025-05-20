0eed90aef02e5cd790efe387b2e87d47235c635d
Bob <support@withtheranks.com>
2025-05-20 13:06:38 +0700

    Remove description from audience list

--- components/audience-card.tsx
+++ components/audience-card.tsx
@@ -20,9 +20,6 @@ export default function AudienceCard({
           <h3 className="my-0 truncate font-cal text-xl font-bold tracking-wide dark:text-white">
             {data.name}
           </h3>
-          <p className="mt-2 line-clamp-2 text-sm font-normal leading-snug text-stone-500 dark:text-stone-400">
-            {data.description || "No description provided."}
-          </p>
         </div>
         <div className="border-t border-stone-200 p-4 pt-2 text-xs text-stone-400 dark:border-stone-700 dark:text-stone-500">
           <div className="flex flex-col gap-1 sm:justify-between">
