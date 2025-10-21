d629684136963d06a7ca72b0135c3949faf850b4
Bob <bob.hoangle@gmail.com>
2025-10-21 18:51:05 +0700

    feat: add user authentication and developers docs

---
+++ pages/docs/_meta.js
@@ -0,0 +1,19 @@
+export default {
+  index: {
+    title: "Introduction",
+  },
+  "-- Users": {
+    type: "separator",
+    title: "For Organizers",
+  },
+  authentication: {
+    title: "Authentication",
+  },
+  "-- Developers": {
+    type: "separator",
+    title: "For Developers",
+  },
+  "api-reference": {
+    title: "API Reference",
+  },
+};
