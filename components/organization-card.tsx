c057eb3f0d1b3ace5bfd9b48efb7cb6c79356836
Bob <bob.hoangle@gmail.com>
2025-12-12 15:08:57 +0700

    refactor: remove unused codeblocks

--- components/organization-card.tsx
+++ components/organization-card.tsx
@@ -6,7 +6,6 @@ import BlurImage from "@/components/blur-image";
 import { placeholderBlurhash } from "@/lib/utils";
 
 export default function OrganizationCard({ data }: { data: Organization }) {
-  // const url = `${data.subdomain}.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`;
   return (
     <div className="relative rounded-lg border border-stone-200 pb-10 shadow-md transition-all hover:shadow-xl dark:border-stone-700 dark:hover:border-white">
       <Link
@@ -32,18 +31,6 @@ export default function OrganizationCard({ data }: { data: Organization }) {
         </div>
       </Link>
       <div className="absolute bottom-4 flex w-full justify-between space-x-4 px-4">
-        {/* <a
-          href={
-            process.env.NEXT_PUBLIC_VERCEL_ENV
-              ? `https://${url}`
-              : `http://${data.subdomain}.localhost:3000`
-          }
-          target="_blank"
-          rel="noreferrer"
-          className="truncate rounded-md bg-stone-100 px-2 py-1 text-base font-medium text-stone-600 transition-colors hover:bg-stone-200 dark:bg-stone-800 dark:text-stone-400 dark:hover:bg-stone-700"
-        >
-          {url} ↗
-        </a> */}
         <Link
           href={`/organization/${data.id}/analytics`}
           className="dark:bg-opacity-50 dark:hover:bg-opacity-50 flex items-center rounded-md bg-green-100 px-2 py-1 text-base font-medium text-green-600 transition-colors hover:bg-green-200 dark:bg-green-900 dark:text-green-400 dark:hover:bg-green-800"
