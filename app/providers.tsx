72a073e4da98a0af4179e4eb462c1e3e0bd84b0f
Bob <bob.hoangle@gmail.com>
2026-01-13 18:28:28 +0700

    feat: add theme system with next-themes

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 
 import { ModalProvider } from "@/components/modal/provider";
@@ -8,8 +9,14 @@ import { ModalProvider } from "@/components/modal/provider";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <Toaster richColors position="bottom-right" />
-      <ModalProvider>{children}</ModalProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem={false}
+      >
+        <Toaster richColors position="bottom-right" />
+        <ModalProvider>{children}</ModalProvider>
+      </ThemeProvider>
     </SessionProvider>
   );
 }
